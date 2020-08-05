8be76ff9ceb6a4256bf6e9d671d8a273d4cc3b57
Prinn Bantoengpratoomrad <prinnb@dunnsolutions.com>
2020-08-05 22:21:04 +0700

    Implemented custom tag to render tvpage meta tags in accelerator storefront. Modified/created controller, jsp, and tags to pass in and populate meta data.

--- core-customize/tvpage/tvpageaddon/acceleratoraddon/web/src/com/tvpage/addon/controllers/pages/TvpageStorefrontController.java
+++ core-customize/tvpage/tvpageaddon/acceleratoraddon/web/src/com/tvpage/addon/controllers/pages/TvpageStorefrontController.java
@@ -4,7 +4,6 @@ import com.tvpagefacades.TvpageStoreFrontFacade;
 import com.tvpagefacades.data.TvpageMetaData;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractPageController;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
-import de.hybris.platform.cms2.model.pages.AbstractPageModel;
 import de.hybris.platform.cms2.model.pages.ContentPageModel;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -15,36 +14,26 @@ import org.springframework.web.servlet.HandlerMapping;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.UnsupportedEncodingException;
 
 @Controller
 @RequestMapping("/storefront/**")
 public class TvpageStorefrontController extends AbstractPageController {
-    private static final Logger log = Logger.getLogger(TvpageStorefrontController.class);
+    private static final Logger LOG = Logger.getLogger(TvpageStorefrontController.class);
     private static final String TVPAGE_STOREFRONT_CMS_PAGE = "tvpageStorefront";
 
     @Resource(name = "tvpageStorefrontFacade")
     private TvpageStoreFrontFacade tvpageStorefrontFacade;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String getAmbassadorPage(final Model model,
-                                    final HttpServletRequest request, final HttpServletResponse response)
-            throws CMSItemNotFoundException, UnsupportedEncodingException {
+    public String getStorefrontPage(final Model model, final HttpServletRequest request) throws CMSItemNotFoundException {
         String restOfTheUrl = (String) request.getAttribute(
                 HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
         final ContentPageModel contentPage = getContentPageForLabelOrId(TVPAGE_STOREFRONT_CMS_PAGE);
         String tvPageHtml = tvpageStorefrontFacade.getTvpageStorefrontHtml("dummy");
-        TvpageMetaData tvPageMetaTags = tvpageStorefrontFacade.getTvpageStorefrontMetaTags("dummy");
+        TvpageMetaData tvpageMetaData = tvpageStorefrontFacade.getTvpageStorefrontMetaTags("dummy");
         model.addAttribute("tvPagehtml", tvPageHtml);
-        //TODO: update meta tags
-        //setUpMetaDataForContentPage(model,contentPage,tvPageMetaTags);
+        model.addAttribute("tvpageMetaData", tvpageMetaData);
         storeCmsPageInModel(model, contentPage);
-        updatePageTitle(model, contentPage);
         return getViewForPage(model);
-    }
-
-    protected void updatePageTitle(final Model model, final AbstractPageModel cmsPage) {
-        storeContentPageTitleInModel(model, getPageTitleResolver().resolveHomePageTitle(cmsPage.getTitle()));
     }
 }
