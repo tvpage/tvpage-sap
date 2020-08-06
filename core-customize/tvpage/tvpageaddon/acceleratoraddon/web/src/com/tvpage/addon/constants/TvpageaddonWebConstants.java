aefded888e2d1cf6ee523af370c8a67b511d04df
Prinn Bantoengpratoomrad <prinnb@dunnsolutions.com>
2020-08-06 15:30:41 +0700

    controller for tvpage storefront page to pass in correct url path to retrieve page html and meta data.

--- core-customize/tvpage/tvpageaddon/acceleratoraddon/web/src/com/tvpage/addon/constants/TvpageaddonWebConstants.java
+++ core-customize/tvpage/tvpageaddon/acceleratoraddon/web/src/com/tvpage/addon/constants/TvpageaddonWebConstants.java
@@ -3,14 +3,10 @@
  */
 package com.tvpage.addon.constants;
 
-/**
- * Global class for all Tvpageaddon web constants. You can add global constants for your extension into this class.
- */
-public final class TvpageaddonWebConstants // NOSONAR
-{
-    private TvpageaddonWebConstants() {
-        //empty to avoid instantiating this constant class
-    }
+public final class TvpageaddonWebConstants {
 
-    // implement here constants used by this extension
+    public static final String TVPAGE_STOREFRONT_CMS_PAGE_URL = "/storefront";
+
+    private TvpageaddonWebConstants() {
+    }
 }
